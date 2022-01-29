cb7b82b130ed1cb7a5f33405715ff80fa0ca504b
masaki-makun <monsterexile@yahoo.co.jp>
2022-01-29 18:56:46 +0900

    File editing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUserSecret,faTrashAlt,faCheckSquare,faCheck } from '@fortawesome/free-solid-svg-icons';
+Vue.component('fa-icon', FontAwesomeIcon);
 
-Vue.config.productionTip = false
+library.add(faUserSecret,faTrashAlt,faCheckSquare,faCheck);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
